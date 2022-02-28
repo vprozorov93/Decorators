f1768dda7cb73678232c3a02d0ba812edb08ab89
Vladislav Prozorov <93047063+vprozorov93@users.noreply.github.com>
2022-02-28 18:20:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ import requests
 import re
 
 
-def decorator(function):
-    def _decorator(path, *args):
-        logging.basicConfig(
-            level=logging.INFO,
-            filemode='a',
-            filename=os.path.join(path, 'log_file.log'),
-            format='[%(asctime)s] %(levelname)s: %(message)s'
-        )
-        logging.info(f'Executing function "{function.__name__}" with arguments: {args}')
-        result = function(*args)
-        logging.info(f'Function "{function.__name__}" return value {result}')
-        return result
+def decorator(path):
+    def _decorator(function):
+        def func_of_decorator(*args):
+            logging.basicConfig(
+                level=logging.INFO,
+                filemode='a',
+                filename=os.path.join(path, 'log_file.log'),
+                format='[%(asctime)s] %(levelname)s: %(message)s'
+            )
+            logging.info(f'Executing function "{function.__name__}" with arguments: {args}')
+            result = function(*args)
+            logging.info(f'Function "{function.__name__}" return value {result}')
+            return result
+
+        return func_of_decorator
 
     return _decorator
 
 
-@decorator
+decorator_path = decorator('.')
+
+
+@decorator_path
 def get_my_ip(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -30,5 +36,4 @@ def get_my_ip(url):
 
 
 if __name__ == '__main__':
-    decorator(get_my_ip('.', 'https://2ip.ru'))
-
+    decorator(get_my_ip('https://2ip.ru'))
